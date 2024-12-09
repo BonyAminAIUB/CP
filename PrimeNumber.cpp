ba3888d916421cdcefe258f660983f1739c87bcb
Bony Amin <bonyamincse@gmail.com>
2024-12-09 21:27:20 +0600

    Update PrimeNumber.cpp

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n = 7;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
     bool isPrime = true;
     for(int i = 2; i<n-1; i++){
         if(n % i == 0){
@@ -9,6 +11,7 @@ int main(){
             break;
         }
     }
+    cout << endl;
     if(isPrime == true){
         cout << "Prime Number";
     }
